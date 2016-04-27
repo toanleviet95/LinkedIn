0633721ae559f53ebba1028cbd03929d81cd0a00
toanleviet95 <toanleviet95@gmail.com>
2016-04-27 17:30:10 +0700

    Third Commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-var app = angular.module("my_app",[]);
-app.controller("my_controller", function($scope){
-    
+var app = angular.module("my_app",['ngSanitize']);
+app.controller("my_controller", function($scope, $http){
+    $http({
+        method: 'GET',
+        url: 'database/data.json'
+    }).success(function(data, status, headers, config){
+        $scope.database = data;
+    }).error(function(data, status, headers, config){
+        alert('Lỗi nhận được dữ liệu từ JSON!');
+    });
 });
